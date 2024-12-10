10c542c571a799dad080a613e9c509e9015afce9
SanjuQA1990 <sanjaykumar.emmbros@gmail.com>
2024-12-10 23:17:12 +0530

    Api Automation 2

--- src/test/java/org/sanjay/EX_Sample/APITesting002.java
+++ src/test/java/org/sanjay/EX_Sample/APITesting002.java
@@ -1,4 +1,8 @@
 package org.sanjay.EX_Sample;
 
 public class APITesting002 {
+    public static void main(String[] args) {
+        System.out.println("Second Program");
+        System.out.println("Second Program");
+    }
 }
